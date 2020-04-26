6b211ccbb7e08f0641cdb4641bb5d7f01ba03697
Junjie Chen <junjie.chen18@gmail.com>
2020-04-26 18:10:08 -0400

    feat(Events): define on method

--- src/models/Events.ts
+++ src/models/Events.ts
@@ -2,4 +2,12 @@ type Callback = () => void;
 
 export class Events {
   events: { [event: string]: Callback[] } = {};
+
+  on = (event: string, callback: Callback): void => {
+    const handlers = this.events[event] || [];
+
+    handlers.push(callback);
+
+    this.events[event] = handlers;
+  };
 }
