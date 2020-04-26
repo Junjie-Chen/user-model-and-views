200dbdd29dbac7ea476c8a223ca5cba0c1bb4d49
Junjie Chen <junjie.chen18@gmail.com>
2020-04-26 18:13:35 -0400

    feat(ApiSync): define save method

--- src/models/ApiSync.ts
+++ src/models/ApiSync.ts
@@ -6,4 +6,14 @@ export class ApiSync {
   fetch(id: number): AxiosPromise {
     return axios.get(`${this.baseUrl}/${id}`);
   }
+
+  save(data): AxiosPromise {
+    const { id } = data;
+
+    if (id) {
+      return axios.put(`${this.baseUrl}/${id}`, data);
+    } else {
+      return axios.post(this.baseUrl, data);
+    }
+  }
 }
