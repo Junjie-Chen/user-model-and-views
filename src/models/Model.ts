6d3c0565e0610792141d0078dfb5f5a066ce9ac9
Junjie Chen <junjie.chen18@gmail.com>
2020-04-26 18:37:05 -0400

    feat(Model): define fetch method

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -1,3 +1,5 @@
+import { AxiosResponse } from 'axios';
+
 export class Model {
   constructor(private attributes, private sync, private events) {}
 
@@ -9,5 +11,22 @@ export class Model {
     this.attributes.set(value);
 
     this.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.get('id');
+
+    if (typeof id !== 'number') {
+      throw new Error('fetch was not successful without an id');
+    }
+
+    this.sync
+      .fetch(id)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data);
+      })
+      .catch(() => {
+        this.trigger('error');
+      });
   }
 }
