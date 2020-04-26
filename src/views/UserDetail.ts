16eca5c95bfe3a7f2704aac892af9c5b49145092
Junjie Chen <junjie.chen18@gmail.com>
2020-04-26 19:54:40 -0400

    feat(UserDetail): define template method

---
+++ src/views/UserDetail.ts
@@ -0,0 +1,11 @@
+export class UserDetail {
+  template(): string {
+    return `
+      <div>
+        <h1>User Detail</h1>
+        <div>User Name: ${this.model.get('name')}</div>
+        <div>User Age: ${this.model.get('age')}</div>
+      </div>
+    `;
+  }
+}
