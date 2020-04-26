ca52ff1aa1b22c34a515021c80e96efae1076660
Junjie Chen <junjie.chen18@gmail.com>
2020-04-26 18:58:38 -0400

    feat(User): define static createUserCollection method

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,11 +1,16 @@
 import { Attributes } from './Attributes';
 import { ApiSync } from './ApiSync';
 import { Events } from './Events';
+import { Collection } from './Collection';
 
 const baseUrl = 'http://localhost:3000/users';
 
 export class User {
   static createUser(data): User {
     return new User(new Attributes(data), new ApiSync(baseUrl), new Events());
+  }
+
+  static createUserCollection(): Collection {
+    return new Collection(baseUrl, User.createUser);
   }
 }
