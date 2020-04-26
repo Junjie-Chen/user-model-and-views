f857dc48ea921d3fa2a5b6a5bed247c018dff2c5
Junjie Chen <junjie.chen18@gmail.com>
2020-04-26 18:13:08 -0400

    feat(ApiSync): define fetch method

--- src/models/ApiSync.ts
+++ src/models/ApiSync.ts
@@ -1,3 +1,9 @@
+import axios, { AxiosPromise } from 'axios';
+
 export class ApiSync {
   constructor(public baseUrl: string) {}
+
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.baseUrl}/${id}`);
+  }
 }
