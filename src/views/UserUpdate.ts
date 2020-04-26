7826e98eab171d03581a80d78f85a3be36f73a78
Junjie Chen <junjie.chen18@gmail.com>
2020-04-26 19:57:23 -0400

    feat(UserUpdate): define onSetNameClick method

---
+++ src/views/UserUpdate.ts
@@ -0,0 +1,11 @@
+export class UserUpdate {
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+
+    if (input) {
+      const name = input.value;
+
+      this.model.set({ name });
+    }
+  };
+}
