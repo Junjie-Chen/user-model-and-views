e15ae8b1645b36f81f20063546305456c1ae4119
Junjie Chen <junjie.chen18@gmail.com>
2020-04-26 19:45:37 -0400

    feat(View): define onModelChange method

--- src/views/View.ts
+++ src/views/View.ts
@@ -2,4 +2,10 @@ export abstract class View {
   regions: { [regionProperty: string]: Element } = {};
 
   constructor(public parent: Element, public model) {}
+
+  onModelChange(): void {
+    this.model.on('change', () => {
+      this.render();
+    });
+  }
 }
