92c7443e080afc5f39fae5d075651b5fcf154609
Junjie Chen <junjie.chen18@gmail.com>
2020-04-26 20:12:39 -0400

    feat(UserCollectionView): extend UserCollectionView class from CollectionView abstract class

--- src/views/UserCollectionView.ts
+++ src/views/UserCollectionView.ts
@@ -1,7 +1,8 @@
+import { CollectionView } from './CollectionView';
 import { UserView } from './UserView';
 import { User } from '../models/User';
 
-export class UserCollectionView {
+export class UserCollectionView extends CollectionView {
   renderView(parent: Element, model: User): void {
     new UserView(parent, model).render();
   }
