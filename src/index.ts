ec6884f57e52d5a129b6e50ba521f6f634d23a45
Junjie Chen <junjie.chen18@gmail.com>
2020-04-26 22:11:20 -0400

    feat(user collection view): save first user to local JSON database

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ import { User } from './models/User';
 const firstUser = User.createUser({ name: 'Jingyan Li', age: 27 });
 
 const secondUser = User.createUser({ name: 'Junjie Chen', age: 30 });
+
+firstUser.save();
