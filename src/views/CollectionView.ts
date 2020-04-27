3eae9800ffad2b3f5c7d0e5c836a6c9925b9331e
Junjie Chen <junjie.chen18@gmail.com>
2020-04-26 20:08:02 -0400

    feat(CollectionView): define render method

--- src/views/CollectionView.ts
+++ src/views/CollectionView.ts
@@ -4,4 +4,20 @@ export abstract class CollectionView {
   constructor(public parent: Element, public collection: Collection) {}
 
   abstract renderView(parent: Element, model): void;
+
+  render(): void {
+    this.parent.innerHTML = '';
+
+    const template = document.createElement('template');
+
+    for (let model of this.collection.models) {
+      const parent = document.createElement('div');
+
+      this.renderView(parent, model);
+
+      template.content.append(parent);
+    }
+
+    this.parent.append(template.content);
+  }
 }
