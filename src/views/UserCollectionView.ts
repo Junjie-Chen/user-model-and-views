72edd0a2dc1247e45c1daffc3f56d2d757b43b26
Junjie Chen <junjie.chen18@gmail.com>
2020-04-26 20:14:09 -0400

    feat(UserCollectionView): use generics for UserCollectionView class

--- src/views/UserCollectionView.ts
+++ src/views/UserCollectionView.ts
@@ -1,8 +1,8 @@
 import { CollectionView } from './CollectionView';
 import { UserView } from './UserView';
-import { User } from '../models/User';
+import { User, Properties } from '../models/User';
 
-export class UserCollectionView extends CollectionView {
+export class UserCollectionView extends CollectionView<User, Properties> {
   renderView(parent: Element, model: User): void {
     new UserView(parent, model).render();
   }
