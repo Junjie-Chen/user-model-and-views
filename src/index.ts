76e614b7876bcc5f6b6b8133493c7b498a602adf
Junjie Chen <junjie.chen18@gmail.com>
2020-04-26 21:08:00 -0400

    feat(user view): create instance of UserView class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 import { User } from './models/User';
+import { UserView } from './views/UserView';
+
+const root = document.getElementById('root');
 
 const user = User.createUser({ name: 'Jingyan Li', age: 27 });
+
+new UserView(root, user);
