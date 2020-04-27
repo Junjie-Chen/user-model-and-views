7464f4efa2fc1df4ddce7558a0163ebc5597a964
Junjie Chen <junjie.chen18@gmail.com>
2020-04-26 20:07:25 -0400

    feat(CollectionView): define renderView abstract method

--- src/views/CollectionView.ts
+++ src/views/CollectionView.ts
@@ -2,4 +2,6 @@ import { Collection } from '../models/Collection';
 
 export abstract class CollectionView {
   constructor(public parent: Element, public collection: Collection) {}
+
+  abstract renderView(parent: Element, model): void;
 }
