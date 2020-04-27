d0bdb19f706e22310a883ac4b13023402810b586
Junjie Chen <junjie.chen18@gmail.com>
2020-04-26 22:20:05 -0400

    feat(user collection view): create instance of UserCollectionView class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { User } from './models/User';
+import { UserCollectionView } from './views/UserCollectionView';
 
 const firstUser = User.createUser({ name: 'Jingyan Li', age: 27 });
 
@@ -9,3 +10,7 @@ firstUser.save();
 secondUser.save();
 
 const userCollection = User.createUserCollection();
+
+const root = document.getElementById('root');
+
+new UserCollectionView(root, userCollection);
